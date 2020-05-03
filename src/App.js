647a50c4d36f0fdb573e7359297cc6d803de56c8
Chris R <45723002+critcey68@users.noreply.github.com>
2020-05-03 10:33:42 -0400

    2

--- src/App.js
+++ src/App.js
@@ -19,9 +19,13 @@ class Board extends React.Component {
     };
     }
 handleClick(i) {
-  const squares = this.state.squares.slice();
-  squares[i] = 'X';
-  this.setState({squares: squares});
+    const squares = this.state.squares.slice();
+  if (calculateWinner(squares) || squares[i])
+  {return;}
+
+  squares[i] = this.state.xIsNext ? 'X' : 'O';
+  this.setState({squares: squares,
+  xIsNext: !this.state.xIsNext,});
 }
 arrayToConsole() {
   console.log("Test");
@@ -38,9 +42,15 @@ arrayToConsole() {
   }
 
   render() {
-    const status = 'Next player: XYZ';
-
-    return (
+    const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner){
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' +
+        (this.state.xIsNext ? 'X' : 'O') ;
+      }
+      return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
@@ -89,4 +99,23 @@ class App extends Component {
 }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 export default App;
